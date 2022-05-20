ab0f942c200a601a59a117501bb8ea3f661bf561
Yoni Cohen <yonicohen1993@gmail.com>
2022-05-20 12:48:41 +0300

    Initial commit

---
+++ app/src/main/java/com/synel/perfectharmony/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.synel.perfectharmony.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.synel.perfectharmony.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
