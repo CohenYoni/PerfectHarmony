f42295c9519d5ff36b222c9a2a0be8719bdbf28d
Yoni Cohen <yonicohen1993@gmail.com>
2022-05-20 13:04:56 +0300

    Update deprecated methods

--- app/src/androidTest/java/com/synel/perfectharmony/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/synel/perfectharmony/ExampleInstrumentedTest.java
@@ -3,8 +3,8 @@ package com.synel.perfectharmony;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -13,13 +13,13 @@ import org.junit.runner.RunWith;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
 
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.synel.perfectharmony", appContext.getPackageName());
     }
